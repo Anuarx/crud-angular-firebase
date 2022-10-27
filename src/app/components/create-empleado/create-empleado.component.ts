ccd856d61a5b238871af348ecca7f1960dfe6df6
Anuarx <anuarcostaa@gmail.com>
2022-10-26 21:09:20 -0300

    Consegui agregar datos a firebase

--- src/app/components/create-empleado/create-empleado.component.ts
+++ src/app/components/create-empleado/create-empleado.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-create-empleado',
   templateUrl: './create-empleado.component.html',
-  styleUrls: ['./create-empleado.component.css']
+  styleUrls: ['./create-empleado.component.css'],
 })
 export class CreateEmpleadoComponent implements OnInit {
+  createEmpleado: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+              private _empleadoService: EmpleadoService) {
+    this.createEmpleado = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      documento: ['', Validators.required],
+      salario: ['', Validators.required]
+    });
   }
 
+  ngOnInit(): void {}
+
+  agregarEmpleado() {
+    this.submitted = true;
+    if(this.createEmpleado.invalid){
+      return
+    }
+
+    const empleado: any = {
+      nombre: this.createEmpleado.value.nombre,
+      apellido: this.createEmpleado.value.apellido,
+      documento: this.createEmpleado.value.documento,
+      salario: this.createEmpleado.value.salario,
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
+    }
+    this._empleadoService.agregarEmpleado(empleado).then(() =>{
+      console.log('empleado registrado con exito');
+    }).catch(error => {
+      console.log(error);
+    })
+  }
 }
