91c18d776212f264038693f1162281d786ca116c
Anuarx <anuarcostaa@gmail.com>
2022-10-31 17:17:10 -0300

    Cambios Finales

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { EmpleadoService } from 'src/app/services/empleado.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-list-empleados',
   templateUrl: './list-empleados.component.html',
-  styleUrls: ['./list-empleados.component.css']
+  styleUrls: ['./list-empleados.component.css'],
 })
 export class ListEmpleadosComponent implements OnInit {
-  items: Observable<any[]>;
+  empleados: any[] = [];
 
-  constructor(firestore: AngularFirestore) {
-    this.items = firestore.collection('items').valueChanges();
-  }
+  constructor(
+    private _empleadoService: EmpleadoService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.getEmpleados();
   }
 
+  getEmpleados() {
+    this._empleadoService.getEmpleados().subscribe((data) => {
+      this.empleados = [];
+      data.forEach((element: any) => {
+        // con esto nos traemos los elementos de la firebase
+        // console.log(element.payload.doc.id); esto para obtener las id en la consola
+        // console.log(element.payload.doc.data()); esto para obtener los datos de la firebase en la consola
+        this.empleados.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data(),
+        });
+      });
+      console.log(this.empleados);
+    });
+  }
+  eliminarEmpleado(id: string) {
+    this._empleadoService
+      .eliminarEmpleado(id)
+      .then(() => {
+        console.log('empleado eliminado con exito');
+        this.toastr.error(
+          'El empleado fue eliminado con exito',
+          'Registro eliminado',
+          {
+            positionClass: 'toast-bottom-right',
+          }
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmpleadoService {
-
-  constructor(private firestore: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore) {}
 
   agregarEmpleado(empleado: any): Promise<any> {
     return this.firestore.collection('empleados').add(empleado);
+  }
+
+  getEmpleados(): Observable<any> {
+    return this.firestore
+      .collection('empleados', (ref) => ref.orderBy('fechaCreacion'))
+      .snapshotChanges();
+  }
+
+  eliminarEmpleado(id: string): Promise<any> {
+    return this.firestore.collection('empleados').doc(id).delete();
+  }
+  getEmpleado(id: string): Observable<any> {
+    return this.firestore.collection('empleados').doc(id).snapshotChanges();
   }
 }
